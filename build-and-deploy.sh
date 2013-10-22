dfb4c96f509423d4e795c3e8b26a059e25987807
Cyril <cyril.picat@gmail.com>
2013-10-22 13:06:49 +0200

    Preparing release 0.3.1

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -4,19 +4,19 @@
 # Build first and check status
 mvn clean install
 if [ "$?" != 0 ]; then
-	echo "Build failed!" 1>&2
+	echo "ERROR - Java build failed!" 1>&2
 	exit $?
 fi
 
 # Run shell tests
 shelltest src/test/shell --execdir --diff
 if [ "$?" != 0 ]; then
-	echo "Shell tests failed!" 1>&2
+	echo "ERROR - Shell tests failed!" 1>&2
 	exit $?
 fi
 
 # Deploy new verion of plugin in Sonar dir
-cp target/sonar-objective-c-plugin-0.3.1-SNAPSHOT.jar $SONARQUBE_HOME/extensions/plugins
+cp target/sonar-objective-c-plugin-0.3.1.jar $SONARQUBE_HOME/extensions/plugins
 
 # Stop/start Sonar
 $SONARQUBE_HOME/bin/macosx-universal-64/sonar.sh stop
